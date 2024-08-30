8c2ff909f5a8a567cc39908fadaa1b63dbde9599
tabpritam <4857209+tabpritam@users.noreply.github.com>
2024-08-31 01:24:02 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const morgan = require("morgan");
+dbConnect();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the API!",
+    status: "success",
+    data: null,
+  });
+});
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
