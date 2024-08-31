5f56c4f9bd6c92521125b61f0b662f98c8a95e3b
tabpritam <4857209+tabpritam@users.noreply.github.com>
2024-08-31 23:40:40 +0530

    refactor: Simplify MongoDB connection code

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
